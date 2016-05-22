b489780c4f6fa17373c07e9f5dec4f09c8819b89
Livoras <livoras@163.com>
2016-05-22 13:41:21 +0800

    test

--- main3.go
+++ main3.go
@@ -36,7 +36,7 @@ func (rr *rot13Reader) Read(b []byte) (int, error) {
 }
 
 func main() {
-	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	s := strings.NewReader("Lbh penpxrq gur pbqr!!")
 	r := rot13Reader{s}
 	io.Copy(os.Stdout, &r)
 }
