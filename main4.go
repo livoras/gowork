8cfdcc408d2671d5885a8ac46adf4c6957e6eb8a
Livoras <livoras@163.com>
2016-05-29 13:04:00 +0800

    channel

--- main4.go
+++ main4.go
@@ -1,15 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	c := make(chan int)
+	// Channel is like a stack
 	go sum(100, 200, c)
 	go sum(1, 3, c)
-	x, y := <- c, <- c
-	fmt.Println(x, y)
+	go sum(1, 34, c)
+	x, y := <-c, <-c
+	fmt.Println(x, y, <-c)
 }
 
 func sum(x, y int, c chan int) {
